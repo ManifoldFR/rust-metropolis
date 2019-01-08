23d7878b7942cdaf06517873c1caf159afbe75ee
ManifoldFR <wilson.jallet@gmail.com>
2019-01-08 19:10:51 +0100

    Update dependencies

--- cffi/bindings.hpp
+++ cffi/bindings.hpp
@@ -1,12 +1,13 @@
+#include <cstdarg>
 #include <cstdint>
 #include <cstdlib>
 
 extern "C" {
 
-// Generates a random walk Markov chain.
+/// Generates a random walk Markov chain.
 void randomWalk(uint32_t n_samples, double x0, double sigma, double *out_buf);
 
-// Generates a distribution sample and copies its data to the output buffer.
+/// Generates a distribution sample and copies its data to the output buffer.
 void sampleMHrandomWalk(uint32_t n_samples, double x0, double *out_buf);
 
 } // extern "C"
