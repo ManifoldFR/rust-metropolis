1c1aff6bfb1ad225a0f94c6bdc916173275ea4c1
ManifoldFR <wilson.jallet@gmail.com>
2019-01-13 15:51:33 +0100

    working pylib

--- pylib/setup.py
+++ pylib/setup.py
@@ -5,15 +5,22 @@ import toml
 cfg = toml.load("Cargo.toml")
 package_meta = cfg['package']
 
+setup_requires = ["setuptools-rust>=0.10.1"]
+
 extensions = [
-    RustExtension("rust_metropolis.string_sum", binding=Binding.PyO3)
+    RustExtension("rust_metropolis.random_walk", binding=Binding.PyO3)
 ]
 
 setup(
     name=package_meta["name"],
     version=package_meta["version"],
-    rust_extensions=extensions,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+    ],
     packages=["rust_metropolis"],
+    rust_extensions=extensions,
+    setup_requires=setup_requires,
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
 )
--- pylib/src/lib.rs
+++ pylib/src/lib.rs
@@ -1,16 +1,22 @@
 #![feature(custom_attribute)]
 
 use pyo3::prelude::*;
+use metropolis;
+use metropolis::random_walk::RandomWalk;
+use rand;
 
+#[pymodule]
+fn random_walk(py: Python, m: &PyModule) -> PyResult<()> {
 
-/// This module is a python module implemented in Rust.
-#[pymodinit]
-fn string_sum(py: Python, m: &PyModule) -> PyResult<()> {
+    #[pyfn(m, "sample_random_walk")]
+    fn sample_rw(n: i64, x0: f64) -> PyResult<Vec<f64>> {
+        let q = RandomWalk::new();
+        let cauchy_x0 = 1.5;
+        let p = |x: f64| 1. / (x - cauchy_x0).powi(2);
 
-    #[pyfn(m, "sum_string")]
-    /// Formats the sum of two numbers as string
-    fn sum_as_string(a: i64, b: i64) -> PyResult<String> {
-        Ok((a + b).to_string())
+        let mhs = metropolis::MHSampler::new(p, &q);
+        let ref mut rng = rand::thread_rng();
+        Ok(mhs.sample(rng, n as usize, x0))
     }
 
     Ok(())
