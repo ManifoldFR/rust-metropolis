87f5650488c05e92c88f85ddfe3043c7ab86c56c
ManifoldFR <wilson.jallet@gmail.com>
2019-01-27 00:41:19 +0100

    re-enable build script

--- build.rs
+++ build.rs
@@ -3,9 +3,9 @@ use std::env;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    //cbindgen::Builder::new()
-    //    .with_crate(crate_dir)
-    //    .generate()
-    //    .expect("Unable to generate bindings.")
-    //    .write_to_file("cffi/bindings.hpp");
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .generate()
+        .expect("Unable to generate bindings.")
+        .write_to_file("cffi/bindings.hpp");
 }
